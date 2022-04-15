b2a791c7b741e282c2134121ea713a3ecd8bcf59
ry-la <ryla@ucsd.edu>
2022-04-15 11:30:39 -0700

    Fixed
    Created an ArrayList that stores openBracket, closeBracket, openParen, and closeParen, and returns if it includes -1.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,20 +11,27 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        int openBracket = markdown.indexOf("[", currentIndex);
-        while(openBracket != -1) {
+        ArrayList<Integer> indexes = new ArrayList<>();
+
+        while(currentIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+            indexes.add(openBracket);
+            indexes.add(closeBracket);
+            indexes.add(openParen);
+            indexes.add(closeParen);
+
+            if(indexes.indexOf(-1) != -1) return toReturn;
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-
-            openBracket = markdown.indexOf("[", currentIndex);
         }
 
         return toReturn;
     }
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
